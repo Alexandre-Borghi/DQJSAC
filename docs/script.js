e551d435380ccfb46037d721b17e9f1134dbe167
Alexandre-Borghi <borghi.alexandre.12@gmail.com>
2022-02-14 01:32:07 +0100

    fixed elapsed time calculations

--- docs/script.js
+++ docs/script.js
@@ -8,17 +8,45 @@ function main() {
 
     function render() {
         const now = new Date();
-        const elapsed = new Date(now - since);
-        const years = elapsed.getFullYear() - 1970;
+        let seconds = now.getSeconds() - since.getSeconds();
+        let minutes = now.getMinutes() - since.getMinutes();
+        let hours = now.getHours() - since.getHours();
+        let days = now.getDate() - since.getDate();
+        let months = now.getMonth() - since.getMonth();
+        let years = now.getFullYear() - since.getFullYear();
+
+        console.log(`${years}/${months}/${days} ${hours}:${minutes}:${seconds}`);
+
+        if (seconds < 0) {
+            seconds = 60 + seconds;
+            minutes -= 1;
+        }
+
+        if (minutes < 0) {
+            minutes = 60 + minutes;
+            hours -= 1;
+        }
+
+        if (hours < 0) {
+            hours = 24 + hours;
+            days -= 1;
+        }
+
+        if (days < 0) {
+            // 31 here is a shortcut because we know October contains 31 days
+            days = 31 + days;
+            months -= 1;
+        }
+
+        if (months < 0) {
+            months = 12 + months;
+            years -= 1;
+        }
+
         const yearsUnit = years <= 1 ? "an" : "ans";
-        const months = elapsed.getMonth();
-        const days = elapsed.getDate();
         const daysUnit = days <= 1 ? "jour" : "jours";
-        const hours = elapsed.getHours();
         const hoursUnit = hours <= 1 ? "heure" : "heures";
-        const minutes = elapsed.getMinutes();
         const minutesUnit = minutes <= 1 ? "minute" : "minutes";
-        const seconds = elapsed.getSeconds();
         const secondsUnit = seconds <= 1 ? "seconde" : "secondes";
 
         const sinceText = `${years} ${yearsUnit},<br>${months} mois,<br>${days} ${daysUnit},<br>${hours} ${hoursUnit},<br>${minutes} ${minutesUnit},<br>${seconds} ${secondsUnit}`;
